4e14db1dfd43834bf71a766e28f4dcdd4d67d68b
Sergei Shchurov <si.shchur@gmail.com>
2023-02-12 23:13:48 +0300

    feat: activity_main layout complete

--- app/src/main/java/com/shchurovsi/repoobserver/MainActivity.kt
+++ app/src/main/java/com/shchurovsi/repoobserver/MainActivity.kt
@@ -2,10 +2,15 @@ package com.shchurovsi.repoobserver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shchurovsi.repoobserver.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
